97dd206eb3817f5a6be380ab886b89940cb05f42
Jonas Napierski <pudeljoni@gmail.com>
2023-04-19 23:40:11 +0200

    mv hyprland to backup

--- autostart.sh
+++ autostart.sh
@@ -27,12 +27,6 @@ function run {
 run sxhkd -c ~/.config/hypr/sxhkd/sxhkdrc &
 # fi
 
-#Some ways to set your wallpaper besides variety or nitrogen
-#feh --bg-scale ~/.config/hypr/wall.png &
-feh --bg-fill /usr/share/backgrounds/archlinux/arch-wallpaper.jpg &
-feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-#wallpaper for other Arch based systems
-#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
 xsetroot -cursor_name left_ptr &
